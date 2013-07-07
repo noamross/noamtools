9e24e8712d7cd615499613281aa505a269085626
Noam Ross <noam.ross@gmail.com>
2013-07-07 07:09:51 -0700

    Changed theme_nr to a proper function, allows changes

--- R/theme_nr.R
+++ R/theme_nr.R
@@ -1,7 +1,9 @@
 #'Noam's default ggplot2 styling
 #'@import ggplot2 grid
 #'@export
-theme_nr <- theme(text=element_text(family="Lato Light", size=14),
+theme_nr <- function(...) {
+  
+  theme(text=element_text(family="Lato Light", size=14),
         panel.grid.major.x=element_blank(),
         panel.grid.minor.x=element_blank(),
         panel.grid.minor.y=element_blank(),
@@ -13,4 +15,6 @@ theme_nr <- theme(text=element_text(family="Lato Light", size=14),
         legend.key.size=unit(1.5, "cm"),
         legend.text=element_text(size=22),
         axis.title=element_text(size=24),
-        axis.text=element_text(color="black",size=13))
+        axis.text=element_text(color="black",size=13)) +
+    theme_update(...)
+}
